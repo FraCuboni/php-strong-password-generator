121830b07e6deea7f463904c2f32a6d0ea13d7bd
FraCuboni <fracuboni@gmail.com>
2024-07-25 16:30:15 +0200

    feat created password generator character checker for double not working

--- landing.php
+++ landing.php
@@ -16,17 +16,42 @@
 
     $alphabet = range('a', 'z');
     foreach ($alphabet as $character) {
-        echo $character;
     }
     $alphabet_big = range('A', 'Z');
     foreach ($alphabet_big as $character) {
-        echo $character;
     }
     $numbers = range('0', '9');
     foreach ($numbers as $character) {
-        echo $character;
+    }
+    $special_characters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', '\\', '|', ';', ':', '\'', '"', ',', '<', '.', '>', '/', '?', '`', '~'];
+    foreach ($special_characters as $character) {
     }
 
+    $all_characters[] = $alphabet;
+    $all_characters[] = $alphabet_big;
+    $all_characters[] = $numbers;
+    $all_characters[] = $special_characters;
+
+    // stampo 12 valori
+    for ($i = 1; $i <= $password_lenght; $i++) {
+
+        // seleziono uno tra i 4 array
+        $array_choser = rand(0, count($all_characters) - 1);
+
+        // ottengo un singolo carattere
+        $single_character = $all_characters[$array_choser][rand(0, count($all_characters[$array_choser]) - 1)];
+
+        $password = [];
+
+        // creo funzioneper non duplicare i caratteri già utilizzati
+        if (!in_array($single_character, $password)) {
+            $password = $single_character;
+        } else {
+            echo 'doppione!' . $single_character;
+        };
+
+        echo $password;
+    }
     ?>
 </body>
 
