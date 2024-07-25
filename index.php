3b287d959d501d4033adb28cdfbe3f7ed300a912
FraCuboni <fracuboni@gmail.com>
2024-07-25 15:49:34 +0200

    feat add basic layput w bootstrap

--- index.php
+++ index.php
@@ -4,16 +4,47 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Strong Password Generator</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 
+<?php
+
+
+?>
+
 <body class="d-flex justify-content-center align-items-center" style="height: 100vh;">
 
-    <div class="container-fluid">
+    <div class="container-fluid d-flex flex-column justify-content-around h-50">
         <div class="row">
             <div class="col-1"></div>
-            <div class="col-10 d-flex justify-content-center align-items-center"></div>
+            <div class="col-10 text-center">
+                <h1>strong password generator</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-1"></div>
+            <div class="col-10">
+
+                <div class="card">
+                    <div class="card-body">
+                        This is some text within a card body.
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-1"></div>
+            <div class="col-10">
+
+                <div class="card">
+                    <div class="card-body">
+                        This is some text within a card body.
+                    </div>
+                </div>
+
+            </div>
         </div>
     </div>
 
